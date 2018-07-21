3640f0d42839e316e6cd927f604835e14115f686
sudan0825@gmail.com <sudan0825@gmail.com>
2018-07-21 09:43:21 -0700

    add image

--- src/components/contents/imageContainer/imageContainer.js
+++ src/components/contents/imageContainer/imageContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import myStyle from './imageContainer.css'
+import myStyle from './imageContainer.css';
+import budweiser from '../../../assets/budweiser.jpeg'
 
 const imageContainer=(props)=>(
     <div className={myStyle.imageContainer}>
-    
-        <div className={myStyle.image}>hhhh</div>
+     
+               
+        <div className={myStyle.image}><img src={budweiser} alt='budweiser'/></div>
                   
    </div>)
     
